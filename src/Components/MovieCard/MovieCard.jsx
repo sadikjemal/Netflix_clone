d8b02236de7ed625ae987187c8cea9028932864e
Sadik <sadikarsi19@gmail.com>
2026-08-19 16:06:35 +0300

    some updates on MovieCard.jsx

--- src/Components/MovieCard/MovieCard.jsx
+++ src/Components/MovieCard/MovieCard.jsx
@@ -1,69 +1,47 @@
 import React from "react";
+import styles from "./MovieCard.module.css";
 import { FaCirclePlay } from "react-icons/fa6";
 import { BsPlusCircle } from "react-icons/bs";
 import { GoCheckCircleFill } from "react-icons/go";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
-import styles from "./MovieCard.module.css";
-function MovieCard({ movie }) {
-  const genres = ["Action", "Thriller", "Adventure"];
+function MovieCard({movie}) {
+     const genres = ["Action", "Thriller", "Adventure"];
   return (
     <div className={styles.cardWrapper}>
       {/* poster image */}
-      <img
-        className={styles.poster}
-        // src={movie.poster_path}
-        alt="poster image"
-      />
+      <  img src={movie.poster_path} alt="poster image" />
 
       {/* hover card */}
-      <div className={styles.hoverCard}>
+
+       <div className={styles.hoverCard}>
         {/* img */}
-        <img
-          className={styles.hoverImage}
-          src={movie.poster_path}
-          alt="hover image"
-        />
+         <img
+                 className={styles.hoverImage}
+                 // src={movie.poster_path}
+                 alt="hover image"
+               />
+
         {/* badge */}
-        {/* <div className={styles.badge}>Recently added</div> */}
-        <div className={styles.badge}>{movie.badge}</div>
+        <div className={styles.badge}>Recently added</div>
 
         {/* button row */}
-       <div className={styles.buttonsRow}>
-          <FaCirclePlay
-            className={styles.circleButton}
-            color="white"
-            size={40}
-          />
-          <BsPlusCircle
-            className={styles.circleButton}
-            color="white"
-            size={40}
-          />
-          <GoCheckCircleFill
-            className={styles.circleButton}
-            color="white"
-            size={40}
-          />
-          <IoIosArrowDropdownCircle
-            className={styles.circleButtonSmall}
-            color="white"
-            size={40}
-          />
+        <div className={styles.buttonsRow}>
+          <FaCirclePlay color="white" size={40}/>
+          <BsPlusCircle color="white" size={40}/>
+          <GoCheckCircleFill  color="white" size={40} />
+          <IoIosArrowDropdownCircle className={styles.circleButtonSmall}  color="white" size={40} />
         </div>
 
         {/* metadata row */}
-     <div className={styles.metaRow}>
-          {/* <span className={styles.tag}> U?/A 16+c</span>
-          <span className={styles.tag}>Movie</span>
-          <span className={styles.tag}>HD</span> */}
-          <span className={styles.tag}>{movie.matureRating}</span>
-<span className={styles.tag}>{movie.category}</span>
-<span className={styles.tag}>{movie.quality}</span>
+       <div className={styles.metaRow}>
+        <span className={styles.tag}>U/A 16+C</span>
+        <span className={styles.tag}> Movie</span>
+        <span className={styles.tag}>HD</span>
         </div>
 
         {/* genres */}
         <div className={styles.genres}>
-          {genres?.map((g, index) => (
+          {genres.map((g, index) => (
             <span key={index}>
               {g}
               {index < genres.length - 1 && (
